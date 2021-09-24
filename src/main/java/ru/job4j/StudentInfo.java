1a3366a53b75f051e3a758d0496d682f5f194359
Artem Kudriavtcev <akudriavtcev@vantage.com>
2021-09-23 20:44:19 -0400

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Artem Kudriavtcev");
+        System.out.println("20210923");
+
+    }
+
+}
